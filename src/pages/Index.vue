d5c82741309304404db85637fdd05f2b2dee88bd
tuannvsys <tuannv@saisystem.vn>
2021-06-28 07:59:26 +0700

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <DateCustom _date=26 _month=6 _year=2021 :_activeDateList="listActiveDateInit" /> -->
+  <DateCustom _date=26 _month=6 _year=2021 :_activeDateList="listActiveDateInit" />
+  
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import DateCustom from "../components/date.vue"
+// import S from "../components/s.vue"
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {
+    DateCustom,
+  },
+  data() {
+    return {
+      listActiveDateInit: ["26-6-2021", "28-6-2021"]
+    }
+  }
+})
+</script>
